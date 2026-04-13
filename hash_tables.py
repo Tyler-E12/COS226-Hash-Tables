222f13675a0aac4d0812a03c47f8b292f43ac561
Tyler-E12 <124187250+Tyler-E12@users.noreply.github.com>
2026-04-13 02:17:19 -0400

    Commit 3 - linear probing bad hash
    updated the hash table to use linear probing for indexing keys

--- hash_tables.py
+++ hash_tables.py
@@ -1,36 +1,51 @@
-# hashtable using linked list
-# each of the indexes in the list stores a bucket
-class LinkedListHashTable:
+# the hash table is using linear probing now
+# each index stores a single item, collissions are handled by moving forward
+class LinearProbingHashTable:
     def __init__(self, size, hash_function):
         self.size = size
-        self.table = [[] for _ in range(size)]
+        # no buckets just slots
+        self.table = [None for _ in range(size)]
         self.hash_function = hash_function
         self.collisions = 0
 
 
-    # this inserts a key-value pair into the hashtable and if muliple items map to the
-    # same index then they are stored in the same bucket (collision detected)
+    # this inserts a key value pair into the hash table and if multiple items map to the
+    # same index then we move forward until an empty spot is found (collision detected)
     def insert(self, key, value):
         index = self.hash_function(key, self.size)
-        bucket = self.table[index]
+        start_index = index
 
-        # counts the collissions (traversing existing elements causes them)
-        if bucket:
-            self.collisions += len(bucket)
+        # counts the collisions (each occupied slot we pass is a collision)
+        while self.table[index] is not None:
+            self.collisions += 1
+            index = (index + 1) % self.size
 
-        bucket.append((key, value))
+            if index == start_index:
+                raise Exception("Hash table is full")
+
+        self.table[index] = (key, value)
+
 
     # searches for a key in the hash table
     def search(self, key):
         index = self.hash_function(key, self.size)
-        bucket = self.table[index]
+        start_index = index
 
-        for k, v in bucket:
+        # loop through table using probing to find key
+        while self.table[index] is not None:
+            k, v = self.table[index]
             if k == key:
                 return v
+
+            index = (index + 1) % self.size
+
+            if index == start_index:
+                break
+
         return None
 
-    # counts to see how many buckets went unused and are empty
+
+    # counts to see how many slots went unused and are empty
     def wasted_space(self):
-        empty_buckets = sum(1 for bucket in self.table if len(bucket) == 0)
-        return empty_buckets
+        empty_slots = self.table.count(None)
+        return empty_slots
