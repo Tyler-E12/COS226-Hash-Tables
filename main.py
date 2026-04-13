1f32809fffc620523b2898cf28a33c1bb7eb0e21
Tyler-E12 <124187250+Tyler-E12@users.noreply.github.com>
2026-04-13 02:56:09 -0400

    Commit 5 - Final Optimization

--- main.py
+++ main.py
@@ -7,9 +7,9 @@
 import time
 from data_load import load_movies
 from hash_tables import LinearProbingHashTable
-from hash_functions import good_hash
+from hash_functions import optimized_hash
 
-table_size = 20000
+table_size = 15000
 
 # this is the function that ACTUALLY builds the hash table with the movie information
 def build_table(table, data, key_type):
@@ -34,14 +34,14 @@ def main():
     movies = load_movies("MOCK_DATA.csv")
 
     # hash table 1 where the title is the key (with linear probing and good hash)
-    title_table = LinearProbingHashTable(table_size, good_hash)
+    title_table = LinearProbingHashTable(table_size, optimized_hash)
     time_title = build_table(title_table, movies, "title")
-    print_stats("Title Key - Linear Probing (Good Linear Probing Hash)", title_table, time_title)
+    print_stats("Title Key - (Final Optimization)", title_table, time_title)
 
     # hash table 2 where the quote is the key (with linear probing and good hash)
-    quote_table = LinearProbingHashTable(table_size, good_hash)
+    quote_table = LinearProbingHashTable(table_size, optimized_hash)
     time_quote = build_table(quote_table, movies, "quote")
-    print_stats("Quote Key - Linear Probing (Good Linear Probing Hash)", quote_table, time_quote)
+    print_stats("Quote Key - Linear Probing (Final Optimization)", quote_table, time_quote)
 
 # what actually makes the program run
 if __name__ == "__main__":
