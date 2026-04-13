e717d884c33a2e3ff87857215b66661e7a51c861
Tyler-E12 <124187250+Tyler-E12@users.noreply.github.com>
2026-04-13 02:20:00 -0400

    Commit 3 - linear probing bad hash
    Changed it so the hash tables use linear probing with a poor hash, by far the worst option so far. There's an incredible amount of both collisions AND wasted space, and I had to make the table size huge just to fit everything (ended up taking 11 seconds for the program to finish)

--- main.py
+++ main.py
@@ -6,47 +6,42 @@
 # space, and build time.
 import time
 from data_load import load_movies
-from hash_tables import LinkedListHashTable
-from hash_functions import poly_hash
+from hash_tables import LinearProbingHashTable
+from hash_functions import bad_hash
 
-table_size = 2000
-
+table_size = 100000
 
 # this is the function that ACTUALLY builds the hash table with the movie information
 def build_table(table, data, key_type):
-    start_time = time.time()
+    start = time.time()
 
     for movie in data:
         key = movie[key_type]
         table.insert(key, movie)
 
-    end_time = time.time()
-    return end_time - start_time
-
+    end = time.time()
+    return end - start
 
 #prints the statistics for each of the hash tables
 def print_stats(name, table, build_time):
     print(name)
     print(f"Collisions: {table.collisions}")
     print(f"Wasted Space: {table.wasted_space()}")
-    print(f"Build Time: {build_time:.6f} seconds")
-    print()  # just a blank line to separate the two tables information
-
+    print(f"Build Time: {build_time:.6f} seconds\n")
 
 
 def main():
-    #loads the movie data from the file
     movies = load_movies("MOCK_DATA.csv")
 
-    # hash table 1 where the title is the key
-    title_table = LinkedListHashTable(table_size, poly_hash)
+    # hash table 1 where the title is the key (now with linear probing
+    title_table = LinearProbingHashTable(table_size, bad_hash)
     time_title = build_table(title_table, movies, "title")
-    print_stats("Hash Table 1 (Title Key - Poly Hash)", title_table, time_title)
+    print_stats("Title Key - Linear Probing (Bad Linear Probing Hash)", title_table, time_title)
 
-    # hash table 2 where the quote is the key
-    quote_table = LinkedListHashTable(table_size, poly_hash)
+    # hash table 2 where the quote is the key (now with linear probing)
+    quote_table = LinearProbingHashTable(table_size, bad_hash)
     time_quote = build_table(quote_table, movies, "quote")
-    print_stats("Hash Table 2 (Quote Key - Poly Hash)", quote_table, time_quote)
+    print_stats("Quote Key - Linear Probing (Bad Linear Probing Hash))", quote_table, time_quote)
 
 # what actually makes the program run
 if __name__ == "__main__":
